86376a2f52cd495b3199a4b0ea301ef75207d5e4
mpshimul <35893908+mpshimul@users.noreply.github.com>
2026-07-06 01:11:16 +0600

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSql } from '@prisma/adapter-libsql';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { createClient } from '@libsql/client';
 
 const dbUrl = process.env.DATABASE_URL || 'file:./db/custom.db';
